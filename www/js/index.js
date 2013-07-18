fbef66cfcaadd9ebb8ffac1f9cc7562ca3907742
tomasell <alex.tomasello@gmail.com>
2013-07-18 11:20:14 +0200

    test table

--- www/js/index.js
+++ www/js/index.js
@@ -12,10 +12,10 @@ function loadRepos() {
         $.each(result.values, function(index, value) {
           console.debug(value.number);
           var tr1 = $('<tr></tr>');
-          tr1.append('<th class="ui-table-priority-1">' + value.number + '</th>');
-          tr1.append('<td class="ui-table-priority-2">' + value.company.name + '</td>');
-          tr1.append('<td class="ui-table-priority-3">' + value.queue + '</td>');
-          tr1.append('<td class="ui-table-priority-4">' + value.receiver.cn + '</td>');
+          tr1.append('<th>' + value.number + '</th>');
+          tr1.append('<td>' + value.company.name + '</td>');
+          tr1.append('<td>' + value.queue + '</td>');
+          tr1.append('<td>' + value.receiver.cn + '</td>');
           tbody.append(tr1);
         });
         $('#dashboard').table('refresh');
