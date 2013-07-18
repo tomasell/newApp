de3e59ab4d66d21e33b342eddc3168b28524565c
tomasell <alex.tomasello@gmail.com>
2013-07-18 11:14:47 +0200

    fixes

--- www/js/index.js
+++ www/js/index.js
@@ -5,28 +5,19 @@ $('#reposHome').bind('pageinit', function(event) {
 });
 
 function loadRepos() {
-  $
-      .ajax('http://10.0.10.67:8888/servicedesk/frontend.json')
-      .done(
-          function(result) {
-            var table = $('#dashboard');
-            var tbody = $(table[0].tBodies);
-            $
-                .each(
-                    result.values,
-                    function(index, value) {
-                      console.debug(value.number);
-                      var tr1 = $('<tr></tr>');
-                      tr1
-                          .append('<th>'
-                              + value.number + '</th>');
-                      tr1.append('<td>' + value.company.name + '</td>');
-                      tr1.append('<td>' + value.queue + '</td>');
-                      tr1.append('<td>' + value.receiver.cn + '</td>');
-                      tbody.append(tr1);
-                    });
-            $('#dashboard').table('refresh');
-          }, function(err) {
-            console.debug(err);
-          });
+  $.ajax('http://10.0.10.67:8888/servicedesk/frontend.json').done(
+      function(result) {
+        var table = $('#dashboard');
+        var tbody = $(table[0].tBodies);
+        $.each(result.values, function(index, value) {
+          console.debug(value.number);
+          var tr1 = $('<tr></tr>');
+          tr1.append('<th class="ui-table-priority-1">' + value.number + '</th>');
+          tr1.append('<td class="ui-table-priority-2">' + value.company.name + '</td>');
+          tr1.append('<td class="ui-table-priority-3">' + value.queue + '</td>');
+          tr1.append('<td class="ui-table-priority-4">' + value.receiver.cn + '</td>');
+          tbody.append(tr1);
+        });
+        $('#dashboard').table('refresh');
+      });
 }
