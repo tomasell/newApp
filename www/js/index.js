aad68ea482134c8764f6728bd3e7c78d8f6b9300
tomasell <alex.tomasello@gmail.com>
2013-07-18 10:50:01 +0200

    dashboard

--- www/js/index.js
+++ www/js/index.js
@@ -5,21 +5,28 @@ $('#reposHome').bind('pageinit', function(event) {
 });
 
 function loadRepos() {
-  $.ajax('http://10.0.10.67:8888/servicedesk/frontend.json').done(
-      function(result) {
-        var table = $('#dashboard');
-        var tbody = $(table[0].tBodies);
-        $.each(result.values, function(index, value) {
-          console.debug(value.number);
-          var tr1 = $('<tr></tr>');
-          tr1.append('<td>' + value.number + '</td>');
-          tr1.append('<td>' + value.company.name + '</td>');
-          tr1.append('<td>' + value.queue + '</td>');
-          tr1.append('<td>' + value.receiver.cn + '</td>');
-          tbody.append(tr1);
-        });
-        $('#dashboard').table('refresh');
-      }, function(err) {
-        console.debug(err);
-      });
+  $
+      .ajax('http://10.0.10.67:8888/servicedesk/frontend.json')
+      .done(
+          function(result) {
+            var table = $('#dashboard');
+            var tbody = $(table[0].tBodies);
+            $
+                .each(
+                    result.values,
+                    function(index, value) {
+                      console.debug(value.number);
+                      var tr1 = $('<tr></tr>');
+                      tr1
+                          .append('<th><a href="http://10.0.10.67:8888//servicedesk/tickets/'
+                              + value.number + '.json/edit</a></th>');
+                      tr1.append('<td>' + value.company.name + '</td>');
+                      tr1.append('<td>' + value.queue + '</td>');
+                      tr1.append('<td>' + value.receiver.cn + '</td>');
+                      tbody.append(tr1);
+                    });
+            $('#dashboard').table('refresh');
+          }, function(err) {
+            console.debug(err);
+          });
 }
